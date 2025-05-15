6534e72500b60247c6c500e6a76dc87d6293ea7e
ftchalo <gdaniel.fuenzalida@gmail.com>
2025-05-15 00:25:37 -0400

    nuevo

--- taller1.1.c
+++ taller1.1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    float suma = 0, cantidad = 0, promedio, mayor, menor, aritmetica, suma_total = 0, suma_diferencias = 0;
-    int C, i, nota;
+    float suma = 0, cantidad = 0, promedio, mayor = 0, menor = 7, aritmetica, suma_total = 0, suma_diferencias, desviacion, d;
+    int i, nota;
     float notas[100];
     printf("\nEscribe 0 cuando termines de ingresar las notas\n\n");
     while (1) {  //bucle infinito
-        printf("Escribe un numero: ");
+        printf("\nEscribe un numero: ");
         scanf("%d", & nota); //leer nota
         if (nota == 0) {
             break; // Salir del bucle si se ingresa 0
@@ -29,18 +29,22 @@ int main() {
          else if (nota < 0 ) {
             printf("\nNota invalida. Debe ser numeros entre 1 y 7\n"); //mensaje de error
         }  
-        if (i >= 100) {
+        if ( cantidad >= 100) {
             printf("\nse ha llegado al maximo de notas\n"); //mensaje de error
         }
-        
     }
-    for(i = 0; i < C; i++){
+    promedio = suma / cantidad; //calcular promedio
+    for(i = 0; i < cantidad; i++){
         suma_diferencias += (notas[i] - promedio) * (notas[i] - promedio);
     }
-    promedio = suma / cantidad; //calcular promedio 
+    desviacion = suma_diferencias / cantidad;
+    d = 0;
+    while (d* d < desviacion) {
+        d = d + 0.000001;
+    }
     printf("\nEl promedio es %f\n", promedio);
     printf("\nLa nota mas grande es %f\n", mayor);
     printf("\nLa nota mas chica es %f\n", menor);
-    printf ("\nLa division estandar es %f\n", (suma_total / C));
+    printf ("\nLa desviacion estandar es %f\n", d);
     return 0; //salir del programa
 }
