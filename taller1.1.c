363fa3a9a2e9d1f02d1bfc7139911794fa06aa42
ftchalo <gdaniel.fuenzalida@gmail.com>
2025-05-15 00:04:47 -0400

    nuevo

--- taller1.1.c
+++ taller1.1.c
@@ -4,16 +4,16 @@ int main() {
     float suma = 0, cantidad = 0, promedio, mayor, menor, aritmetica, suma_total = 0, suma_diferencias = 0;
     int C, i, nota;
     float notas[100];
-    printf("Escribe 0 cuando termines de ingresar las notas");
+    printf("\nEscribe 0 cuando termines de ingresar las notas\n\n");
     while (1) {  //bucle infinito
-        printf("Escribe un número: ");
+        printf("Escribe un numero: ");
         scanf("%d", & nota); //leer nota
         if (nota == 0) {
             break; // Salir del bucle si se ingresa 0
         }
         if (nota > 0 && nota <= 7) { //validar nota
-            printf("Nota valida\n"); //mesaje de nota valida 
-            printf("La nota es %f\n", nota);
+            printf("\nNota valida\n"); //mesaje de nota valida 
+            printf("\nLa nota es %d\n", nota);
             suma = suma + nota; //sumar 1 a la cantidad de notas totales
          if (nota > mayor) {
             mayor = nota; //nota mayor
@@ -24,23 +24,23 @@ int main() {
           cantidad++; //suamr uno a la cantidad de notas totales
         }
          else if (nota > 7) {
-            printf("Nota invalida. Debe ser numeros entre 1 y 7\n"); //mensaje de error
+            printf("\nNota invalida. Debe ser numeros entre 1 y 7\n"); //mensaje de error
         }
-         else if (nota < 0) {
-            printf("Nota invalida. Debe ser numeros entre 1 y 7\n"); //mensaje de error
+         else if (nota < 0 ) {
+            printf("\nNota invalida. Debe ser numeros entre 1 y 7\n"); //mensaje de error
         }  
         if (i >= 100) {
-            printf("se ha llegado al maximo de notas\n"); //mensaje de error
+            printf("\nse ha llegado al maximo de notas\n"); //mensaje de error
         }
         
     }
     for(i = 0; i < C; i++){
         suma_diferencias += (notas[i] - promedio) * (notas[i] - promedio);
     }
-    promedio = suma / cantidad; //calcular promedio
-    suma_total = suma_total + (nota - promedio) * (nota - promedio); 
-    printf("El promedio es %f", promedio);
-    printf("La nota más grande es %f", mayor);
-    printf("La nota más chica es %f", menor);
+    promedio = suma / cantidad; //calcular promedio 
+    printf("\nEl promedio es %f\n", promedio);
+    printf("\nLa nota mas grande es %f\n", mayor);
+    printf("\nLa nota mas chica es %f\n", menor);
+    printf ("\nLa division estandar es %f\n", (suma_total / C));
     return 0; //salir del programa
 }
