c4e0b64d8f1c6e27c4a7295dfb6a06a8c6533a0a
Gonzalo Fuenzalida <gdaniel.fuenzalida@gmail.com>
2026-08-23 16:01:30 -0400

    casos + reglas

--- Ejercicio2.pl
+++ Ejercicio2.pl
@@ -12,3 +12,16 @@ sala(sala_1, 10, [pizarra]).
 sala(sala_2, 20, [pizarra]).
 sala(sala_3, 50, [pizarra]).
 
+%2. al no existir funciones en prolog se crearan "reglas" las cuales contendran.
+% el catalogo. flujo_solicitudes(Solicitudes, Aceptado, Rechazao):-.
+flujo_solicitudes(Solicitudes, Aceptado, Rechazao):-
+    %creamos otra regla, la cual estará dentro de la regla ya creada para así se active cuando llamemos a la anterior.
+    porcesar_solicitud(Solicitudes, [], Aceptado, Rechazao).
+
+%ahora pasaremos a creear los 2 casos.
+
+%caso 1: no hay más solicitudes.
+porcesar_fluujo([], _, [], []).
+
+%caso 2: procesar solicitud siguiente (recursivo).
+porcesar_fluujo([solicitud(ReqID, Franja, Asistentes, ReqEquip) | Resto], EstadoActual, Asignadas, Rechazadas) :-
